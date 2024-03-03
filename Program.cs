b832f8e1b129056798e83331970780a5428a920a
S. David Kyle <sdavidkyle@gmail.com>
2024-03-03 16:51:31 -0700

    adding a new project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace observer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class MessageEventArgs : EventArgs
+{
+    public string Message { get; set; }
+    public MessageEventArgs(string message)
+    {
+        Message = message;
+    }
+}
+
+public class EventBasedObservable
+{
+    public event EventHandler<MessageEventArgs>? Event;
+    public void FireEvent(string message)
+    {
+        Event?.Invoke(this, new(message));
+    }
+}
+
